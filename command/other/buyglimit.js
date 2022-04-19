5c55d45cbe2fa9fe4a8ea4e4e3a1fb60caa01f03
Bumble Bee VR <51755226+bumblebeevr@users.noreply.github.com>
2022-04-20 05:02:02 +0700

    Add files via upload

--- command/other/buyglimit.js
+++ command/other/buyglimit.js
@@ -1,16 +1,21 @@
 module.exports = {
-  name: "buyglimit",
-  category: "other",
-  isSpam: true,
+	name: "buyglimit",
+	category: "other",
+	isSpam: true,
 	use: "<angka>",
-  query: "Kirim perintah *#buyglimit* jumlah limit yang ingin dibeli\n\nHarga 1 limit game = $150 balance",
-  async run(msg, conn, q) {
-                if (q.includes('-')) throw `Jangan menggunakan -`
-                if (isNaN(q)) throw `Harus berupa angka`
-                let ane = Number(Math.floor(q) * 150)
-                if (getBalance(msg.sender, balance) < ane) throw `Balance kamu tidak mencukupi untuk pembelian ini`
-                kurangBalance(msg.sender, ane, balance)
-                givegame(sender, nebal(args[1]), glimit)
-                await msg.reply(`Pembeliaan game limit sebanyak ${q} berhasil\n\nSisa Balance : $${getBalance(sender, balance)}\nSisa Game Limit : ${cekGLimit(sender, gcount, glimit)}/${gcount}`)
-  }
-  }
+	query: "Send an order *#buyglimit* the limit amount you want to buy\n\nPrice for 1 game limit = $150 balance",
+	async run(msg, conn, q) {
+		if (q.includes("-")) throw `Don't use -`;
+		if (isNaN(q)) throw `Must be a number`;
+		let ane = Number(Math.floor(q) * 150);
+		if (getBalance(msg.sender, balance) < ane) throw `Your balance is not sufficient for this purchase`;
+		kurangBalance(msg.sender, ane, balance);
+		givegame(msg.sender, Math.floor(q), glimit);
+		await msg.reply(
+			`Purchase of game limit of ${q} was successful\n\nRemaining balance : $${getBalance(
+				msg.sender,
+				balance
+			)}\nRemaining Game Limit : ${cekGLimit(msg.sender, gcount, glimit)}/${gcount}`
+		);
+	},
+};
