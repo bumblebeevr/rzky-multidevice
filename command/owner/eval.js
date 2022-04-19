6863fc323e3ec85bd41cd33ac55a2c8205922822
Bumble Bee VR <51755226+bumblebeevr@users.noreply.github.com>
2022-04-20 05:03:05 +0700

    Add files via upload

--- command/owner/eval.js
+++ command/owner/eval.js
@@ -1,15 +1,17 @@
 module.exports = {
 	name: "eval",
-	alias: [">>", "ev"],
+	alias: [">>", ">"],
 	category: "private",
+	noPrefix: true,
+	isOwner: true,
 	desc: "running javascript code via command can also test something code",
 	use: `">" <code javascript> with await and ">>" <code> live return or immediately show the result`,
-	async run(msg, conn, q, isOwner, body, map, config, args, arg) {
-		if (!isOwner) return;
-		let kode = body.trim().split(/ +/);
+	query: `Masukan Parameter Code`,
+	async run(msg, conn, q, map, args, arg) {
+		let kode = msg.body.trim().split(/ +/)[0];
 		let teks;
 		try {
-			teks = await eval(`(async () => { ${q}})()`);
+			teks = await eval(`(async () => { ${kode == ">>" ? "return" : ""} ${q}})()`);
 		} catch (e) {
 			teks = e;
 		} finally {
