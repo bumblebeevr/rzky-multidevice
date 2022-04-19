f3271108c07be1a9d169d8bf23ac14c9f0d0259c
Bumble Bee VR <51755226+bumblebeevr@users.noreply.github.com>
2022-04-20 05:09:10 +0700

    Add files via upload

--- command/umum/help.js
+++ command/umum/help.js
@@ -2,18 +2,18 @@ module.exports = {
 	name: "help",
 	alias: ["h", "cmd", "menu"],
 	category: "umum",
-	async run(msg, conn, q, isOwner, body, map, config, args) {
+	isLimit: true,
+	async run(msg, conn, q, map, args) {
 		if (q) {
 			const data = [];
 			const name = q.toLowerCase();
 			const { command, prefix } = map;
-			const cmd = command.get(name) || [...command.values()].find((x) => x.alias.find((x) => x == args[0]));
-			if (!cmd || cmd.category === "private" && !isOwner) return await msg.reply("Command tidak ditemukan");
+			const cmd = command.get(name) || [...command.values()].find(x => x.alias.find(x => x == args[0]));
+			if (!cmd || (cmd.category === "private" && !config.owner.includes(msg.sender))) return await msg.reply("Command tidak ditemukan");
 			else data.push(`*Nama:* ` + cmd.name);
 			if (cmd.alias) data.push(`*Alias:* ${cmd.alias.join(", ")}`);
 			if (cmd.desc) data.push(`*Deskripsi:* ${cmd.desc}`);
-			if (cmd.use)
-				data.push(`*Penggunaan:* ${prefix}${cmd.name} ${cmd.use}\n\nCatatan: [] = opsional, | = atau, <> = harus diisi`);
+			if (cmd.use) data.push(`*Penggunaan:* ${prefix}${cmd.name} ${cmd.use}\n\nCatatan: [] = opsional, | = atau, <> = harus diisi`);
 
 			return await msg.reply(data.join("\n"));
 		} else {
@@ -27,37 +27,43 @@ module.exports = {
 				if (!cmd) continue;
 				if (config.ignore.directory.includes(info.category.toLowerCase())) continue;
 				cteg = info.category || "No Category";
-				if (!cteg || cteg === "private") cteg = 'owner command';
+				if (!cteg || cteg === "private") cteg = "owner command";
 				if (Object.keys(category).includes(cteg)) category[cteg].push(info);
 				else {
 					category[cteg] = [];
 					category[cteg].push(info);
 				}
 			}
-			let str =
-				"```" +
-				config.namebot +
-				"```\n\n" +
-				`Halo, ${pushName === undefined ? sender.split("@")[0] : pushName}\n*Disini List Commandnya*\n\n`;
+			let str = "```" + config.namebot + "```\n\n" + `Halo, ${pushName === undefined ? sender.split("@")[0] : pushName}\n*Disini Daftar Commandnya*\n\n`;
 			const keys = Object.keys(category);
+			//var a = 1
 			for (const key of keys) {
-				str += `*${key.toUpperCase()}*\n${category[key].map((cmd) => `• *${cmd.name}*`).join("\n")}\nPenggunaan : *#${
-					category[key].map((cmd) => cmd.name)[0]
-				}*\n\n`;
+				str += `==== [ *${key.toUpperCase()}* ] ====\n${category[key]
+					.map(
+						cmd =>
+							`➤ *${cmd.name}*\n*⋙ Alias:* \n${cmd.alias.map(a => `_*▸* ${a || "Tidak ada"}_`).join("\n")}\n*⋙ Use:* _*${cmd.use || "No Parameter"}*_\n*⋙ Use Prefix:* *_${
+								cmd.options.noPrefix ? "❎" : "✅"
+							}_*  \n`
+					)
+					.join("\n")}\n\n`;
 			}
 			str += `typing *${prefix}help sticker* for get the details and example use`;
 			await conn.sendMessage(
 				msg.from,
-				{
-					text: str,
+				{ 
+					video: await conn.getBuffer(config.thumbvideo),
+					gifPlayback: true,
+					caption: str,
 					footer: config.namebot + " • " + config.ownername,
 					templateButtons: [
-						{ urlButton: { displayText: "Report Bug", url: "https://chat.rzkyfdlh.tech" } },
-						{ urlButton: { displayText: "Downloader Website", url: "https://downloader.rzkyfdlh.tech" } },
+					  { urlButton: { displayText: "Shortlink", url: "https://sl.rzkyfdlh.tech" } },
+						{ urlButton: { displayText: "Downloader", url: "https://downloader.rzkyfdlh.tech" } },
+						{ quickReplyButton: {displayText: 'Script Bot📑', id: '#script'}},
+            { quickReplyButton: {displayText: 'Owner👥', id: '#owner'}},
 					],
 				},
 				{ quoted: msg }
 			);
 		}
 	},
-}; 
+};
